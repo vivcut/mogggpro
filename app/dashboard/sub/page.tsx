94bcc5ec6ded9d052bf78e6e8b1dbd2a6413461b
Vivaan Kumar <vivaan@Vivaans-MacBook-Pro.local>
2026-06-06 22:01:34 +0800

    fix

--- app/dashboard/sub/page.tsx
+++ app/dashboard/sub/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { UserContext } from "@/app/rootprovider";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
